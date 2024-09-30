6e3c6e91b5cadab7b323a28d0f7c4ca3b8b64a46
cammu02 <alexcammar04@gmail.com>
2024-09-30 08:39:35 +0200

    HolaMon en distintos lenguajes

---
+++ c/holamon.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hola mon!\n");
+    return 0;
+}
---
+++ php/holamon.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP </title>
+</head>
+<body>
+</body>
+</html>
